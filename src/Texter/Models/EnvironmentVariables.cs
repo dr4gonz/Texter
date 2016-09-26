125d7710d775141c38d573f8497d92d802b245a9
Matt Reyes Ben Logue <none@none.com>
2016-09-26 10:02:24 -0700

    add initial files and Message.cs

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "ACb4144136d7e68bf288ff485807b583b1";
+        public static string AuthToken = "d3ccfd4371c4d3d7ef5b7b4986c87291";
+    }
+}
